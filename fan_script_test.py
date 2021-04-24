3dc3cf32967f363c498b414eb09e811e1b35dd30
Tim Schaefer <tim.schaefer.de@gmail.com>
2021-04-24 10:52:36 +0200

    new config

--- fan_script_test.py
+++ fan_script_test.py
@@ -2,7 +2,15 @@ from pyfan import *
 
 pwm_list = range(1, 5)
 for pwm_number in pwm_list:
-    set_fanspeed(hwmon7, pwm_number, "10")
+    set_fanspeed(hwmon7, pwm_number, "200")
     reset_fan_config(hwmon7, pwm_number)
 
-# plot_fan_speeds(hwmon7)
+
+#plot_fan_speeds(hwmon7,[3])
+
+while True:
+    set_fanspeed(hwmon7, 1, "255")
+    set_fanspeed(hwmon7, 2, "255")
+    set_fanspeed(hwmon7, 3, "255")
+    set_fanspeed(hwmon7, 4, "255")
+    time.sleep(0.1)
